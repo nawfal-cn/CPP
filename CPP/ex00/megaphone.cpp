8dd37f1adb09d65abd262faa8a0cd6e5f708ef0a
Nawfal1chaknan <61191519+Nawfal1chaknan@users.noreply.github.com>
2023-11-11 12:23:34 +0100

    Add files via upload

--- CPP/ex00/megaphone.cpp
+++ CPP/ex00/megaphone.cpp
@@ -3,25 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nchaknan <nchaknan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 19:26:27 by nchaknan          #+#    #+#             */
-/*   Updated: 2023/09/24 22:44:57 by nchaknan         ###   ########.fr       */
+/*   Updated: 2023/10/30 19:37:10 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<iostream>
+#include <iostream>
 
 int main(int ac, char **av){
-    int j = 0;
-    
-    if(ac == 1){
+    if(ac == 1)
         std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-    }
-    while(av[++j]){
+
+    std::string str;
+    int j = 0;
+    while(av[++j])
+    {
+        str = av[j];
         int i = -1;
-        while(av[j][++i])
-            std::cout<<(char)std::toupper(av[j][i]);
+        while(str[++i])
+        
+            std::cout<<(char)std::toupper(str[i]);
     }
     std::cout<<'\n';
     return(0);
--- CPP/ex01/phonebook.cpp
+++ CPP/ex01/phonebook.cpp
@@ -3,15 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   phonebook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nchaknan <nchaknan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/24 23:10:10 by nchaknan          #+#    #+#             */
-/*   Updated: 2023/09/24 23:13:07 by nchaknan         ###   ########.fr       */
+/*   Updated: 2023/10/31 15:38:26 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<iostream>
+#include <string>
 
-int main{
+int main ()
+{
+    std::string fstName, lstName, number;
     
+    std::cout<<"First Name: ";
+    std::getline(std::cin, fstName);
+    std::cout<<"Last Name: ";
+    std::getline(std::cin, lstName);
+    std::cout<<"Number: ";
+    std::getline(std::cin, number);
+
+    std::cout<<"Your first Name is "<<fstName<<".\n";
+    std::cout<<"Your last Name is "<<lstName<<".\n";
+    std::cout<<"Your number is "<<number<<".\n";
 }
